b53c75079a6ab2fa5f44f1e95172600255a9cefd
ivannz <ivannz@yandex.ru>
2019-07-09 20:15:43 +0300

    TST tensor-ring reassembly test

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -29,3 +29,17 @@ def test_tensor_to_tt_and_back(random_state):
     assert a == 1 and z == 1
 
     assert all(core.shape[1] == b for b, core in zip(body, cores))
+
+
+def test_tr_circularity(random_state):
+    ranks = [3, 2, 3, 5]
+    shape = [2, 3, 7, 5], [3, 7, 5, 2]
+
+    cores = []
+    for r0, n, m, r1 in zip(ranks[-1:] + ranks[:-1], *shape, ranks):
+        cores.append(torch.randn(r0, n, m, r1, dtype=torch.double))
+
+    base = tensor.tr_to_tensor(*cores, k=0)
+    for k in range(1, len(cores)):
+        res = tensor.tr_to_tensor(*cores, k=k)
+        assert torch.allclose(res, base)
